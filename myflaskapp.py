f250518fd72c06e887ed5fb13e514a976144f9a9
coursemdetw <course@mde.tw>
2016-03-08 21:49:40 +0800

    add init.py to initialize data directories

--- myflaskapp.py
+++ myflaskapp.py
@@ -2,6 +2,8 @@
 from flask import Flask, send_from_directory, request, redirect, render_template, session, make_response
 import random
 import os
+# init.py 為自行建立的起始物件
+import init
 
 # 確定程式檔案所在目錄, 在 Windows 有最後的反斜線
 _curdir = os.path.join(os.getcwd(), os.path.dirname(__file__))
@@ -14,6 +16,9 @@ else:
     # 表示程式在近端執行
     data_dir = _curdir + "/local_data/"
     static_dir = _curdir + "/static"
+
+# 利用 init.py 啟動, 建立所需的相關檔案
+initobj = init.Init()
     
 app = Flask(__name__)
 
@@ -22,46 +27,6 @@ app = Flask(__name__)
 # set the secret key.  keep this really secret:
 app.secret_key = 'A0Zr9@8j/3yX R~XHH!jmN]LWX/,?R@T'
 
-def __init__():
-    # 建立必要的目錄
-    # hope to create downloads and images directories　
-    if not os.path.isdir(data_dir+"images"):
-        try:
-            os.makedirs(data_dir+"images")
-        except:
-            print("images mkdir error")
-    if not os.path.isdir(data_dir+"downloads"):
-        try:
-            os.makedirs(data_dir+"downloads")
-        except:
-            print("downloads mkdir error")
-    if not os.path.isdir(data_dir+"db"):
-        try:
-            os.makedirs(data_dir+"db")
-        except:
-            print("db mkdir error")
-    # 建立資料庫檔案
-    '''
-    try:
-        conn = sqlite3.connect(data_dir+"db/database.db")
-        cur = conn.cursor()
-        # 建立資料表
-        cur.execute("CREATE TABLE IF NOT EXISTS entries( \
-                id INTEGER PRIMARY KEY AUTOINCREMENT, \
-                name TEXT not null, \
-                category TEXT not null, \
-                knownby TEXT not null, \
-                memo TEXT);")
-        cur.close()
-        conn.close()
-    except:
-        print("can not create db and table")
-    with closing(connect_db()) as db:
-        with app.open_resource('schema.sql' , mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-    '''
-        
 @app.route("/")
 def index():
     #這是猜數字遊戲的起始表單, 主要在產生答案, 並且將 count 歸零
